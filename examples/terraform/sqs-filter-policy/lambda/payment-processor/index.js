4c9cb1ded0fe66831bd21a7cceab0859408022a8
aircwo <aircwo@gmail.com>
2026-03-30 09:59:12 +0100

    chore: remove examples from the repository

--- examples/terraform/sqs-filter-policy/lambda/payment-processor/index.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-  for (const record of event.Records) {
-    const body = JSON.parse(record.body);
-    console.log('[payment-processor] received payment:', JSON.stringify(body));
-  }
-  return { statusCode: 200 };
-};
